dcaca0a9ae71762e8a7648f5a1b629b799366fbd
Jeroen Doggen <jeroendoggen@gmail.com>
2013-11-24 15:12:38 +0100

    Added temple for 'new filters'

---
+++ Filter/examples/newFilter/newFilter.ino
@@ -0,0 +1,26 @@
+// Arduino Signal Filtering Library
+// Copyright 2012-2013 Jeroen Doggen (jeroendoggen@gmail.com)
+
+#include <newFilter.h>
+
+newFilter Filter;
+
+int value;
+int filtered;
+
+void setup()
+{
+  Serial.begin(115200);
+  Filter.begin();
+}
+
+void loop()
+{
+  value = analogRead(A0);
+  filtered= Filter.run(value);
+  Serial.print("In: ");
+  Serial.print(value);
+  Serial.print(" - Out: ");
+  Serial.println(filtered);
+  delay(500);                                     //make it readable
+}
---
+++ Filter/newFilter.h
@@ -0,0 +1,20 @@
+// Arduino Signal Filtering Library
+// Copyright 2012-2013 Jeroen Doggen (jeroendoggen@gmail.com)
+
+#ifndef newFilter_h
+#define newFilter_h
+#include <Arduino.h>
+#include <Filter.h>
+
+class newFilter : public Filter
+{
+  public:
+    newFilter();
+    void begin();
+
+    int run(int data);
+
+  private:
+    int _y[3];
+};
+#endif
